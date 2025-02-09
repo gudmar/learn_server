1127c94f33f12f237c2410c5d171ea7620969ff5
Marek Gudalewicz <marek.gudalewicz@localhost>
2025-02-09 01:39:25 +0100

    Register user works

--- DataStorage/userManagement/saveUser.js
+++ DataStorage/userManagement/saveUser.js
@@ -1,13 +1,14 @@
 const { getStoragePath, USERS } = require("./dataStorageNames")
-const { add } = require('./dbInterface')
+const { add, get } = require('./dbInterface')
 
-const getUserByLogin = async(login) => (user) => user.login === login
+const getUserByLogin = async (login) => 
+    get(getStoragePath(USERS), (content) => 
+            content.find((user) => user.login === login))
 
 const rejesterUser = async ({ name, nickName, password }) => {
     const path = getStoragePath(USERS);
     const user = { name, login: nickName, password }
-    const userWithTheSameLogin = await getUserByLogin(nickName)(user)
-    console.log(userWithTheSameLogin)
+    const userWithTheSameLogin = await getUserByLogin(nickName)
     if (userWithTheSameLogin !== undefined) {
         return {
             result: false,
